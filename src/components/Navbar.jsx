1c54f69cdb1de5f467e7a8614a9c86a9587bfad7
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-12-12 00:51:09 +0100

    responsive navbar is added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className=' w-full h-16 md:h-20 flex items-center justify-between'>
+        {/* LOGO */}
+        <div className=' flex items-center gap-4 text-2xl font-bold'>
+            <img src="/logo.png" className=' w-8 h-8' alt="" />
+            <span>lamalog</span>
+        </div>
+
+        {/* MOBILE MENU */}
+        <div className=' md:hidden'>
+            M
+        </div>
+
+        {/* DESKTOP MENU */}
+        <div className=' hidden md:flex'>
+            D
+        </div>
+    </div>
   )
 }
 
