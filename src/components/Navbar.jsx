1f833434cdeca05efe8353ac831f0a8ab5c6875c
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-12-12 15:17:50 +0100

    Hamburger menu and navbar is added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
+
   return (
     <div className=' w-full h-16 md:h-20 flex items-center justify-between'>
         {/* LOGO */}
@@ -11,12 +13,32 @@ const Navbar = () => {
 
         {/* MOBILE MENU */}
         <div className=' md:hidden'>
-            M
+            {/* Mobile Button */}
+            <div className=' cursor-pointer text-4xl' onClick={() => setOpen(open => !open)}>
+                {open ? "X" : "="}
+            </div>
+            {/* Mobile Link Lists */}
+            <div className={ `w-full h-screen flex flex-col justify-center items-center absolute top-16 bg-[#e6e6ff] transition-all 
+            ease-in-out gap-8 font-medium text-lg ${open ? "-right-0" : "-right-[100%]"}`}>
+                <a href="">Home</a>
+                <a href="">Trending</a>
+                <a href="">Most Popular</a>
+                <a href="">About</a>
+                <a href="">
+                    <button className=' py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login 👋</button>
+                </a>
+            </div>
         </div>
 
         {/* DESKTOP MENU */}
-        <div className=' hidden md:flex'>
-            D
+        <div className=' hidden md:flex items-center gap-8 xl:gap-12 font-medium'>
+            <a href="">Home</a>
+            <a href="">Trending</a>
+            <a href="">Most Popular</a>
+            <a href="">About</a>
+            <a href="">
+                <button className=' py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login 👋</button>
+            </a>
         </div>
     </div>
   )
