5ce98b00532899879abdc5ae8ed08d33c616c57a
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-12-13 14:50:20 +0100

    React Router Dom new method is added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,43 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './routes/HomePage.jsx'
+import PostLists from './routes/PostLists.jsx'
+import Write from './routes/Write.jsx'
+import LoginPage from './routes/LoginPage.jsx'
+import Register from './routes/Register.jsx'
+import SinglePostPage from './routes/SinglePostPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/posts",
+    element: <PostLists />
+  },
+  {
+    path: "/:slug",
+    element: <SinglePostPage />
+  },
+  {
+    path: "/write",
+    element: <Write />
+  },
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
