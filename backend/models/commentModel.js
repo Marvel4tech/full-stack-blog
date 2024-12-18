a9542a39f422beffac5c5a09f7941388d956cbdb
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-12-18 03:59:10 +0100

    MongoDb SChema and Models are added

---
+++ backend/models/commentModel.js
@@ -0,0 +1,24 @@
+import { Schema } from "mongoose";
+import mongoose from "mongoose";
+
+const commentSchema = ({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    post: {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true
+})
+
+const Comment = mongoose.model("Comment", commentSchema)
+export default Comment
