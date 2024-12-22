5a999f0696101ac38a800f79bb3ede1d3c2a1419
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-12-22 20:29:28 +0100

    React Query setup is added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,6 +10,9 @@ import Register from './routes/Register.jsx'
 import SinglePostPage from './routes/SinglePostPage.jsx'
 import MainLayouts from './layouts/MainLayouts.jsx'
 import { ClerkProvider } from '@clerk/clerk-react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 // Import your Publishable Key
 const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -55,7 +58,9 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={publishableKey}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ClerkProvider>
   </StrictMode>,
 )
