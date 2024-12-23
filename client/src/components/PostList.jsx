2266176e1ae74a2729e476ac71befb0c21492613
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-12-23 13:06:08 +0100

    commit

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -3,11 +3,13 @@ import PostListItem from './PostListItem'
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 
+// fetch Post Funtion that is being used inside the react query below
 const fetchPost = async () => {
   const res = await axios.get(`${import.meta.env.VITE_API_URL}/posts`)
   return res.data
 }
 
+// React Query
 const PostList = () => {
   const { isPending, error, data } = useQuery({
     queryKey: ['repoData'],
@@ -19,6 +21,7 @@ const PostList = () => {
   if (error) return 'An error has occurred: ' + error.message
 
   console.log(data)
+  //
 
   return (
     <div className=' flex flex-col gap-12 mb-8'>
